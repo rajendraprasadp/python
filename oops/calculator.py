f3ea0b3776f3bac4fdad449e4b243647adef72c2
rajendraprasadp <rajuprasadp1@gmail.com>
2021-10-05 14:42:35 +0530

    init

--- oops/calculator.py
+++ oops/calculator.py
@@ -13,7 +13,7 @@ class Calculator:
 
     @staticmethod
     def greet():
-        print("********************hello*******************")
+        print("********************helertewrewrewrwerwerwerewrewlo*******************")
 
 
 hel = Calculator(4)
